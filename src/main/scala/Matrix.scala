c010bb33410e28bdcd80ac8a75f561c411ebd413
karank85 <kkjas85@gmail.com>
2022-07-17 14:59:28 +0700

    added init computation for sparse matrix and inverse for dense matrix

--- src/main/scala/Matrix.scala
+++ src/main/scala/Matrix.scala
@@ -1,11 +1,12 @@
-import scala.collection.parallel.CollectionConverters._
+import java.util.concurrent.atomic.AtomicInteger
+import scala.collection.parallel.CollectionConverters.*
 import scala.collection.mutable.ArrayBuffer
 
 object Matrix extends App {
 
   class DenseMatrix(data: ArrayBuffer[ArrayBuffer[Int]]) {
 
-    private val mt: ArrayBuffer[ArrayBuffer[Int]] = data;
+    private val mt: ArrayBuffer[ArrayBuffer[Int]] = data
     private val m = mt.length
     private val n = mt.head.length
 
@@ -44,7 +45,6 @@ object Matrix extends App {
           (0 until n).par.foreach(j => if mt(i)(j) != thatData(i)(j) then return false)
         })
         true
-
     }
 
     def transpose: DenseMatrix = {
@@ -65,39 +65,81 @@ object Matrix extends App {
 
     def isInvertible: Boolean = determinant != 0
 
-    def inverse: DenseMatrix = if isInvertible then this * createIdentity(mt.length) else this
+    def inverse: DenseMatrix = {
+      if isInvertible then
+        val dt = determinant
+        val newMatrix: ArrayBuffer[ArrayBuffer[Int]] = ArrayBuffer.fill(m, n)(0)
+        (0 until m).par.foreach(i => {
+          (0 until n).par.foreach(j => newMatrix(i)(j) = dt * mt(j)(i))
+        })
+        DenseMatrix(newMatrix)
+      else
+        throw Exception("Matrix is not invertible!")
+    }
 
     def createIdentity(n: Int): DenseMatrix = ???
 
     def getMatrixArray: ArrayBuffer[ArrayBuffer[Int]] = mt
   }
 
-  class SparseMatrices(data: ArrayBuffer[ArrayBuffer[Int]]) {
+  class SparseMatrix(data: ArrayBuffer[ArrayBuffer[Int]]) {
 
-    private val mt: ArrayBuffer[ArrayBuffer[Int]] = data;
-    private val m = mt.length
-    private val n = mt.head.length
+    private val m = data.length
+    private val n = data.head.length
+    private val size = data.par.map(_.par.count(_ != 0)).sum
+    private val mt = computeSparseMatrix
+
+    def computeSparseMatrix: ArrayBuffer[ArrayBuffer[Int]] = {
+      val mt = ArrayBuffer.fill(3,size)(0)
+      val counter = AtomicInteger(0)
+      (0 until m).par.foreach(i => {
+        (0 until n).par.foreach(j =>{
+          val elem = data(i)(j)
+          if elem != 0 then
+            val row = counter.get()
+            mt(0)(row) = i
+            mt(1)(row) = j
+            mt(2)(row) = elem
+            counter.getAndIncrement()
+        })
+      })
+      mt
+
+    }
+
+    def getMatrixArray: ArrayBuffer[ArrayBuffer[Int]] = mt
 
   }
 
-  val mt1 = ArrayBuffer.fill(2048,2048)(1)
+
+  val mt1 = ArrayBuffer.fill(2048, 2048)(1)
   val mt2 = ArrayBuffer.fill(2048,2048)(5)
-  val mt3 = ArrayBuffer(ArrayBuffer(1,2),ArrayBuffer(3,4))
-  val mt4 = ArrayBuffer(ArrayBuffer(1,2),ArrayBuffer(3,4))
+  val mt3 = ArrayBuffer.tabulate(50)(i => ArrayBuffer.tabulate(30)(j => (i+1)*(j+2)))
+  val mt4 = ArrayBuffer.tabulate(50)(i => ArrayBuffer.tabulate(30)(j => i+j))
 
   val m1 = new DenseMatrix(mt1)
   val m2 = new DenseMatrix(mt2)
   val m3 = new DenseMatrix(mt3)
   val m4 = new DenseMatrix(mt4)
 
+  val sm1 = ArrayBuffer(ArrayBuffer(0,0,3,0,4),ArrayBuffer(0,0,5,7,0),ArrayBuffer(0,0,0,0,0),ArrayBuffer(0,2,6,0,0))
+
+  val s1 = new SparseMatrix(sm1)
+
+  println(sm1)
+  println(s1.getMatrixArray)
+
 
   val start1 = System.nanoTime()
-  //val res = m2*m1
   val addition = m3 + m4
   val end1 = (System.nanoTime()-start1)/1e9d
-  println("Runtime: " + end1)
-  println(addition.getMatrixArray)
-  println(m3 == m1)
+  //println("Runtime: " + end1)
+  //println(addition.getMatrixArray)
+
+
+  //println(m3 == m1)
+  //println(m3.determinant)
+
 
 
   //println((m2 * m1).getMatrixArray)
